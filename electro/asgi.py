6bad9ac3cb2dafb2018956a826d228571d4ee1ef
Polixa14 <igorpolxx@gmail.com>
2023-07-14 17:24:52 +0200

    nginx

--- electro/asgi.py
+++ electro/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for electo project.
+ASGI config for electro project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'electo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'electro.settings')
 
 application = get_asgi_application()
